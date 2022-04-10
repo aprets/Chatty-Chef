3ff9c0f4dfac8d3ce54da5cca7050b1edeb003ef
Shui <shui.clouds@gmail.com>
2022-04-10 13:26:25 +0100

    feat: generated types for menu sections

--- next/lib/types/generated/contentful.d.ts
+++ next/lib/types/generated/contentful.d.ts
@@ -34,7 +34,113 @@ export interface IExampleCowPageType extends Entry<IExampleCowPageTypeFields> {
   };
 }
 
-export type CONTENT_TYPE = "exampleCowPageType";
+export interface IMenuItemTypeFields {
+  /** Name */
+  name: string;
+
+  /** Description */
+  description?: string | undefined;
+
+  /** Price */
+  price: number;
+
+  /** Calories */
+  calories: number;
+
+  /** Picture */
+  picture?: Asset | undefined;
+}
+
+/** Content-type for menu item */
+
+export interface IMenuItemType extends Entry<IMenuItemTypeFields> {
+  sys: {
+    id: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+    locale: string;
+    contentType: {
+      sys: {
+        id: "menuItemType";
+        linkType: "ContentType";
+        type: "Link";
+      };
+    };
+  };
+}
+
+export interface IMenuPageTypeFields {
+  /** Content Item Title */
+  contentItemTitle: string;
+
+  /** Main Title */
+  mainTitle: string;
+
+  /** Subtitle */
+  subtitle?: string | undefined;
+
+  /** Header Picture */
+  headerPicture: Asset;
+
+  /** Menu Sections */
+  menuSections: IMenuSection[];
+}
+
+/** Content-type for the menu page */
+
+export interface IMenuPageType extends Entry<IMenuPageTypeFields> {
+  sys: {
+    id: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+    locale: string;
+    contentType: {
+      sys: {
+        id: "menuPageType";
+        linkType: "ContentType";
+        type: "Link";
+      };
+    };
+  };
+}
+
+export interface IMenuSectionFields {
+  /** Title */
+  title: string;
+
+  /** Description */
+  description?: string | undefined;
+
+  /** Items */
+  items: IMenuItemType[];
+}
+
+/** Content-type for a section on menu, must have a title and list of food */
+
+export interface IMenuSection extends Entry<IMenuSectionFields> {
+  sys: {
+    id: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+    locale: string;
+    contentType: {
+      sys: {
+        id: "menuSection";
+        linkType: "ContentType";
+        type: "Link";
+      };
+    };
+  };
+}
+
+export type CONTENT_TYPE =
+  | "exampleCowPageType"
+  | "menuItemType"
+  | "menuPageType"
+  | "menuSection";
 
 export type LOCALE_CODE = "en-US";
 
