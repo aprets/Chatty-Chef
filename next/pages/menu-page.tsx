1189886e27f63b1cd0882f8e9fc3b915f7dda69e
Shui <shui.clouds@gmail.com>
2022-04-11 11:14:08 +0100

    feat(menu-page): added section navbar to menu page, fetches tags from menu items

--- next/pages/menu-page.tsx
+++ next/pages/menu-page.tsx
@@ -1,53 +1,54 @@
-import {Container, Text, Image, Title, SimpleGrid, Stack, Tabs} from '@mantine/core'
+import {Container, Text, Image, Title, SimpleGrid, Box} from '@mantine/core'
 import {GetStaticProps} from 'next'
 import {getPageDataById} from '../lib/contentful'
 import {IMenuPageTypeFields} from '../lib/types/generated/contentful'
 import {MenuItem, OffsetDiv, SectionNavbar} from '../components/Menu'
 
 export default function MenuPage({data}: {data: IMenuPageTypeFields}) {
-	const tabNames = data.menuSections.map((s) => s.fields.title)
 	return (
-		<Container>
-			<Title order={1}>{data.mainTitle}</Title>
-			{data.subtitle && <Text style={{paddingTop: 5, paddingBottom: 10}}> {data.subtitle} </Text>}
-			<Image radius='md' src={data.headerPicture.fields.file.url} alt={data.headerPicture.fields.title} />
-			<Tabs py={10}>
-				{
-					tabNames.map((name) => (
-						<Tabs.Tab key={name} label={name}> </Tabs.Tab>
-					))
-				}
-			</Tabs>
-			{data.menuSections.map(({fields: sectionData}) => (
-				<>
-					<Stack my={20}>
-						<Title id={`section-${sectionData.title}`} order={2}>{sectionData.title}</Title>
-						{sectionData.description && <Text color='gray'>{sectionData.description}</Text>}
-					</Stack>
-					<SimpleGrid
-						cols={3}
-						spacing='lg'
-						breakpoints={[
-							{maxWidth: 'md', cols: 3, spacing: 'md'},
-							{maxWidth: 'sm', cols: 2, spacing: 'sm'},
-							{maxWidth: 'xs', cols: 1, spacing: 'sm'},
-						]}
-					>
-						{
-							sectionData.items.map(({sys, fields: {name, price, calories, description, picture}}) => (
-								// <Grid.Col md={5} lg={4} key={sys.id}>
-								<MenuItem
-									{...{name, price, calories, description}}
+		<>
+
+			<Image src={data.headerPicture.fields.file.url} alt={data.headerPicture.fields.title} height='15vh' />
+			<SectionNavbar sectionNames={data.menuSections.filter((s) => s.fields).map((s) => s.fields.title)} />
+			<Container size='xl' mt={10}>
+
+				{data.menuSections.filter((s) => s.fields).map(({fields: sectionData}) => (
+					<>
+						<Box my={20}>
+							<OffsetDiv id={`section-${sectionData.title}`} />
+							<Title order={2}>{sectionData.title}</Title>
+							{sectionData.description && <Text mt={5} color='gray'>{sectionData.description}</Text>}
+						</Box>
+						<SimpleGrid
+							cols={4}
+							spacing='lg'
+							breakpoints={[
+								{maxWidth: 'md', cols: 3, spacing: 'md'},
+								{maxWidth: 'sm', cols: 2, spacing: 'sm'},
+								{maxWidth: 'xs', cols: 1, spacing: 'sm'},
+							]}
+						>
+							{
+								sectionData.items.filter((i) => i.fields).map(({
+									sys,
+									fields: {name, price, calories, description, picture},
+									metadata: {tags},
+								}) => (
+									<MenuItem
+										key={sys.id}
+										{...{name, price, calories, description}}
 										pictureUrl={picture.fields?.file?.url}
 										pictureAlt={picture.fields?.title}
-								// </Grid.Col>
-							))
-						}
-					</SimpleGrid>
-				</>
-			))}
+										tags={tags.map((t) => t.sys.id.slice(7))}
+									/>
+								))
+							}
+						</SimpleGrid>
+					</>
+				))}
+			</Container>
 			<pre>{JSON.stringify(data, null, 4)}</pre>
-		</Container>
+		</>
 	)
 }
 
