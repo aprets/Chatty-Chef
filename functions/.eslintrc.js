503a50fcdc755926a32eb4557b5a250dc80c72eb
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-04-12 00:10:07 +0100

    chore: fix linter

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
 		project: './tsconfig.json',
         tsconfigRootDir: __dirname,
 	},
-    extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'plugin:@typescript-eslint/recommended'],
+    extends: ['airbnb-typescript/base', 'plugin:@typescript-eslint/recommended'],
     env: {
         browser: true,
         node: true,
@@ -34,27 +34,9 @@ module.exports = {
         '@typescript-eslint/object-curly-spacing': ['error', 'never'],
         'object-curly-newline': 0,
     
-        'react/jsx-one-expression-per-line': 0,
-        'react/no-access-state-in-setstate': 0,
-        'react/destructuring-assignment': 0,
-        'react/jsx-props-no-spreading': 0,
-        'react/jsx-indent': [2, 'tab'],
-        'react/no-array-index-key': 0,
-        'react/react-in-jsx-scope': 0,
-        'react/jsx-indent-props': 0,
-        'react/no-danger': 0,
         /* SPICES & HERBS */
-        'react/require-default-props': 0,
-        'jsx-quotes': ['error', 'prefer-single'],
         "@typescript-eslint/explicit-module-boundary-types": "off",
     
-        'jsx-a11y/no-noninteractive-element-interactions': 0,
-        'jsx-a11y/no-static-element-interactions': 0,
-        'jsx-a11y/click-events-have-key-events': 0,
-        'jsx-a11y/media-has-caption': 0,
-        'jsx-a11y/anchor-is-valid': 0,
-        'jsx-a11y/no-noninteractive-tabindex': 0,
-        'jsx-a11y/no-interactive-element-to-noninteractive-role': 0,
     
         'global-require': 0,
     
