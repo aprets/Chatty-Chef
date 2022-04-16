57c0810c6e657fa6b4a12dc16900ef5b8e3b8a15
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-04-16 17:56:37 +0100

    refactor: streamline chatbot api

--- next/components/Chatbot/api.ts
+++ next/components/Chatbot/api.ts
@@ -3,7 +3,7 @@ const API = {
 		setTimeout(() => {
 			if (message === 'hi') resolve('Hello I am the Chatty Chef, how could I help you?')
 			else resolve(`echo : ${message}`)
-		}, 2000)
+		}, 1000)
 	}),
 }
 
